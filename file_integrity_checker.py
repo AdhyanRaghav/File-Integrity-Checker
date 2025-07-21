08789c60ba672d23e618fcdda6e0627c0757fb40
AdhyanRaghav <adhyanraghav@gmail.com>
2025-07-21 15:58:41 +0530

    Update file_integrity_checker.py

--- file_integrity_checker.py
+++ file_integrity_checker.py
@@ -6,3 +6,16 @@ import os        # To check file existence and paths
 # Function: calculate_hash
 # Purpose : To generate a unique hash of a file's content using SHA-256
 # ---------------------------------------------------
+
+def calculate_hash(file_path):
+    sha256 = hashlib.sha256()  # Create a SHA256 hash object
+
+    with open(file_path, "rb") as file:  # Open file in binary mode
+        while True:
+            chunk = file.read(4096)      # Read file in chunks of 4KB
+            if not chunk:
+                break
+            sha256.update(chunk)         # Update the hash with each chunk
+
+    return sha256.hexdigest()            # Return the final hash string
+
